748d02f0647ba4baa78988dae3877e406412322f
DusanJovanovic <jovanovic.dusan@yahoo.com>
2018-02-28 04:19:50 +0100

    Fc

---
+++ club.py
@@ -0,0 +1,7 @@
+class Club():
+
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
+    
+    
---
+++ main.py
@@ -0,0 +1,5 @@
+import club
+
+juventus = club.Club('Juventus', 'ITA')
+
+print(juventus.name, juventus.country)
